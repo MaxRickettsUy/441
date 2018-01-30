134eec95e6448044d1d6fd9ff8e9f22bac960204
Max Ricketts-Uy <ricke1@umbc.edu>
2018-01-30 14:31:40 -0500

    removed excess whitespace

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-
 #pseudocode
 #INSERTION-SORT(A)				cost	times 
 #for j = 2 to A.length				c1 	n
@@ -12,7 +11,6 @@ from __future__ import print_function
 #		A[i+1] = A[i]			c6	summation, j=2 to n, t (j subscript)
 #		i = i - 1			c7 	summation, j=2 to , t(j subscript) - 1
 #	A[i+1] = key				c8 	n - 1
-
 
 if __name__ == "__main__":
 	an_array = [5,3,2,4,1]
