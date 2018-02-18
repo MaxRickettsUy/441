55be3740f47939663b71918482ce1653ea4a6853
Max Ricketts-Uy <ricke1@umbc.edu>
2018-02-17 20:51:46 -0500

    implemented test_buildMaxHeap

--- test_heap.py
+++ test_heap.py
@@ -17,15 +17,15 @@ class TestHeap(unittest.TestCase):
         self.obj1 = Object(len(array),array)
         # print(self.obj1.getArray())
 
-    # def test_LEFT(self):
-    #     # print("test_LEFT")
-    #     self.assertEqual(heapSort.LEFT(2),4)
-    #     self.assertEqual(heapSort.LEFT(7),14)
+    def test_LEFT(self):
+        # print("test_LEFT")
+        self.assertEqual(heapSort.LEFT(2),5)
+        self.assertEqual(heapSort.LEFT(7),15)
 
-    # def test_RIGHT(self):
-    #     # print("test_RIGHT")
-    #     self.assertEqual(heapSort.RIGHT(2),5)
-    #     self.assertEqual(heapSort.RIGHT(7),15)
+    def test_RIGHT(self):
+        # print("test_RIGHT")
+        self.assertEqual(heapSort.RIGHT(2),6)
+        self.assertEqual(heapSort.RIGHT(7),16)
 
     def test_heapMax(self):
         array = [1,2,3,4,5,6,7,8,9,10]
@@ -36,11 +36,16 @@ class TestHeap(unittest.TestCase):
     def test_maxHeapify(self):
         array = [3,1,8,7,9,4,2,6,5,10]
         maxHeap = [8,1,4,7,9,3,2,6,5,10]
-        random.shuffle(array)
         self.obj1 = Object(len(array),array)
         heapSort.maxHeapify(self.obj1,0)
-        self.assertItemsEqual(self.obj1.getArray(),maxHeap)
+        self.assertSequenceEqual(self.obj1.getArray(),maxHeap)
 
+    def test_buildMaxHeap(self):
+        array = [3,1,8,7,9,4,2,6,5,10]
+        maxHeap = [10,9,8,7,3,4,2,6,5,1]
+        self.obj1 = Object(len(array),array)
+        heapSort.buildMaxHeap(self.obj1)
+        self.assertSequenceEqual(self.obj1.getArray(),maxHeap)
 
 if __name__ == '__main__':
     unittest.main()
