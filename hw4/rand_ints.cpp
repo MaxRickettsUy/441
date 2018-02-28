e3298021d2dffecef6961fcd19b96939c315e0bf
Max Ricketts-Uy <ricke1@umbc.edu>
2018-02-27 21:31:11 -0500

    replaced params

--- hw4/rand_ints.cpp
+++ hw4/rand_ints.cpp
@@ -6,7 +6,6 @@
 #include <cstdlib>
 
 vector<int> generate_random_ints(int num_ints){
-
   vector<int> ints_vect;
   for(int i = 0; i < num_ints; i++){
     ints_vect.push_back(std::rand() % num_ints);
